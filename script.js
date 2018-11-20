0be9a7278aeb0a62180a4bf93df68f864e3d54bd
bigbeno37 <bigbeno37@gmail.com>
2018-11-20 18:29:00 +1100

    User can make the number go up, and add number go uppers

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let number = 0;
 
+let autoNumberVersion = 1;
+let autoNumberCost = 10;
+
+let autoNumberInterval;
+
 function numberGoUp() {
     number++;
 
@@ -7,18 +12,30 @@ function numberGoUp() {
 }
 
 function buyAutoNumber() {
-    // TODO
+    if (number < autoNumberCost) return;
+
+    number -= autoNumberCost;
+
+    autoNumberVersion++;
+    autoNumberCost = Math.ceil(autoNumberCost*1.3);
+
+    clearInterval(autoNumberInterval);
+
+    autoNumberInterval = setInterval(() => {
+        number++;
+        update();
+    }, 1000/(autoNumberVersion - 1));
+
+    update();
 }
 
 function update() {
     document.getElementById("number").innerText = number;
 
-    // TODO: Convert to number
-    if (document.getElementById("auto-number-cost").innerText >= number) {
-        document.getElementById("auto-number").setAttribute("disabled", "false");
-    } else {
-        document.getElementById("auto-number").setAttribute("disabled", "true");
-    }
+    document.getElementById("number-per-second").innerText = (autoNumberVersion-1).toFixed(2);
 
+    document.getElementById("auto-number-version").innerText = autoNumberVersion;
+    document.getElementById("auto-number-cost").innerText = autoNumberCost;
 
+    document.getElementById("auto-number").disabled = autoNumberCost > number;
 }
